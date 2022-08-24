e96ee2f1520581597264555063abf454abcf91df
EugenefedorovPro <eugene.proskulikov@gmail.com>
2022-08-24 18:37:41 +0300

    readme added

--- put_stress_rus/utils/word_processing.py
+++ put_stress_rus/utils/word_processing.py
@@ -1,4 +1,3 @@
-from wiktionary_rus.wiktionary import wiki_instances
 import numpy as np
 from memoization import cached
 
@@ -9,13 +8,11 @@ class WordsProcessing():
     @classmethod
     @cached
     def get_unique_chs_from_unstressed_words(cls):
-      unique_chs_from_unstressed_words = set()
-      for item in wiki_instances:
-        if item.status:
-          chs = list(item.word_lowcase)
-          unique_chs_from_unstressed_words.update(chs)
-      unique_chs_from_unstressed_words = list(unique_chs_from_unstressed_words)
-      unique_chs_from_unstressed_words.sort()
+      unique_chs_from_unstressed_words = ['-', 'а', 'б', 'в', 'г', 'д', 'е', 'ж',
+                                          'з', 'и', 'й', 'к', 'л', 'м', 'н', 
+                                          'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 
+                                          'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 
+                                          'э', 'ю', 'я', 'ё']
       return unique_chs_from_unstressed_words
 
 
