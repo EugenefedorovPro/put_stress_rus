f2544d28f397da3fcd889a2b8015af7b9ea25e0a
EugenefedorovPro <eugene.proskulikov@gmail.com>
2022-08-27 18:36:48 +0300

    predict verbose=0

--- put_stress_rus/put_stress.py
+++ put_stress_rus/put_stress.py
@@ -1,5 +1,6 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 import tensorflow as tf
 from put_stress_rus.utils.word_processing import WordsProcessing
@@ -7,7 +8,7 @@ from tensorflow.keras.models import load_model
 from pathlib import Path
 
 
-path_model_stress = Path(__file__).parent / "model_put_stress.h5" 
+path_model_stress = Path(__file__).parent / "model_put_stress.h5"
 model_stress = load_model(path_model_stress)
 print("Trained Model uploaded successfully")
 
@@ -18,12 +19,13 @@ def errors(func):
             return func(*args)
         except KeyError:
             return "Input word includes invalid character, remove it and try again"
+
     return inner
 
 
 @errors
 def put_stress(word_lowcase):
     word_as_number = WordsProcessing.word2numbers(word_lowcase)
-    stress_as_array = model_stress.predict(word_as_number) 
+    stress_as_array = model_stress.predict(word_as_number, verbose=0)
     word_stressed = WordsProcessing.numbers2word_stressed(stress_as_array, word_lowcase)
-    return  word_stressed
+    return word_stressed
